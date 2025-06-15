69d84579bd0025200c4f704633e6b914475e2a62
amylbranson <amy.branson@csuglobal.edu>
2025-06-15 17:39:49 -0400

    Add files via upload

---
+++ RestaurantInfo.java
@@ -0,0 +1,7 @@
+public class RestaurantInfo {
+    public static void main(String[] args) {
+        System.out.println("Sunny Side Café");
+        System.out.println("123 Main Street");
+        System.out.println("Pittsburgh, PA 15222");
+    }
+}
